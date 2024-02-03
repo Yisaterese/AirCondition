4fe14988b96c9fccca0dd0611af080ec8dc69fb9
User <you@example.com>
2024-02-03 22:23:29 +0100

    project

--- src/AirCondition.java
+++ src/AirCondition.java
@@ -1,32 +1,74 @@
 public class AirCondition {
     private boolean isOn = false;
-    private int temperature;
+    private int temperature = 16;
 
     public boolean isOn() {
-        return isOn = true;
+        return isOn;
     }
 
-    public boolean isOff() {
-        boolean isOff = false;
-        return false;
+    public void isOff() {
+        isOn = false;
     }
 
-    public boolean increaseTemperature() {
-        temperature++;
-        return true;
+    public boolean toggleSwitch() {
+        isOn = !isOn;
+        return isOn;
     }
 
-    public boolean decreaseTemperature() {
-        temperature--;
-        return false;
-    }
-
-    public boolean ifIIncreaseTemperatureBeyondRangeItIsStillTheSame(int number) {
-        boolean theRangeOfTemperature = temperature < 16 && temperature > 30;
-        if(increaseTemperature() < theRangeOfTemperature){
-        }else{
-
+    public void increaseTemperature(int number) {
+        for (int increase = 1; increase <= number; increase++) {
+            temperature++;
+            //return temperature;
         }
-        return true;
+
     }
+
+    public int getIncreaseTemperature() {
+        return temperature;
+    }
+
+    public void decreaseTemperature(int number) {
+        for (int decrease = number; decrease >= 1; decrease--) {
+            this.temperature--;
+        }
+    }
+
+    public int getDecrasetemperature() {
+        return temperature;
+    }
+
+    public void sametTemperatureRange(int number) {
+        for (int increase = 1; increase <= number; increase++) {
+            boolean temperatureIsLessThanOrGreaterthan30 = temperature < 30;
+            if (temperatureIsLessThanOrGreaterthan30) {
+                temperature++;
+            } else {
+
+            }
+        }
+    }
+
+    public int getSameTemperatureRange() {
+        return temperature;
+
+    }
+
+    public void setSametTemperatureRange(int decrement) {
+        for (int decrease = decrement; decrease >= 1; decrease--) {
+            boolean temperatureIsLessThanOrGreaterthan30 = decrement < temperature;
+            if (temperatureIsLessThanOrGreaterthan30) {
+                temperature--;
+            } else {
+
+            }
+        }
+    }
+
+    public int getReturnSameTemperatureRange() {
+        return temperature;
+
+    }
+
+
+
 }
--- src/AirConditionTest.java
+++ src/AirConditionTest.java
@@ -5,39 +5,68 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AirConditionTest {
 
     @Test
-    public void airConditionisOn_airConditionOnTest() {
+    public void airConditionIsOn_airConditionOnTest() {
         AirCondition aC = new AirCondition();
+        assertFalse(aC.isOn());
+        assertTrue(aC.toggleSwitch());
         assertTrue(aC.isOn());
-        aC.isOn();
-        assertTrue(aC.isOn());
-
-    }
-        @Test
+    }       @Test
         public void airConditionIsOff_airConditionIsOffTest() {
         AirCondition aC = new AirCondition();
+        assertFalse(aC.isOn());
+        assertTrue(aC.toggleSwitch());
         assertTrue(aC.isOn());
-        aC.isOff();
-        assertFalse(aC.isOff());
+        assertFalse(aC.toggleSwitch());
+        //aC.isOff();
     }
-    @Test
+       @Test
     public void airConditionTemperatureIncrease_airConditionTemperatureIncreaseTest() {
         AirCondition aC = new AirCondition();
-        assertFalse(aC.isOff());
-        aC.isOn();
+        assertFalse(aC.isOn());
+        assertTrue(aC.toggleSwitch());
         assertTrue(aC.isOn());
-        aC.increaseTemperature();
-        assertTrue(aC.increaseTemperature());
+        aC.increaseTemperature(5);
+        assertEquals(21,aC.getIncreaseTemperature());
+
     }
     @Test
-    public void airConditionTemperatureDecrease_airConditionTemperatureDecreaseTest(){
+    public void airConditionTemperatureDecrease_airConditionTemperatureDecreaseTest() {
         AirCondition aC = new AirCondition();
-        assertFalse(aC.decreaseTemperature());
+        assertFalse(aC.isOn());
+        assertTrue(aC.toggleSwitch());
+        assertTrue(aC.isOn());
+        aC.increaseTemperature(5);
+        assertEquals(21, aC.getIncreaseTemperature());
+        aC.decreaseTemperature(10);
+        assertEquals(11, aC.getDecrasetemperature());
+
     }
     @Test
-    public void airConditionTemperature30_airConditionTemperatureIs30(){
+    public void temperatureIncreasedTo30_temperatureincreaseTo30Test() {
         AirCondition aC = new AirCondition();
-        assertFalse(aC.decreaseTemperature());
-        aC.increaseTemperatureBeyondRange(15);
-        assertEquals(15,aC.increaseTemperatureBeyondRange());
+        assertFalse(aC.isOn());
+        assertTrue(aC.toggleSwitch());
+        assertTrue(aC.isOn());
+        aC.increaseTemperature(9);
+        assertEquals(25, aC.getIncreaseTemperature());
+        aC.sametTemperatureRange(40);
+        System.out.println(aC.getSameTemperatureRange());
+        assertEquals(30,aC.getSameTemperatureRange());
+
     }
+    @Test
+    public void temperatureDecreasedTo16_temperatureDecreaseTo16Test() {
+        AirCondition aC = new AirCondition();
+        assertFalse(aC.isOn());
+        assertTrue(aC.toggleSwitch());
+        assertTrue(aC.isOn());
+        aC.increaseTemperature(14);
+        assertEquals(30, aC.getIncreaseTemperature());
+       aC.setSametTemperatureRange(-1);
+       System.out.println(aC.getReturnSameTemperatureRange());
+        assertEquals(30,aC.getReturnSameTemperatureRange());
+
+    }
+
+
 }
