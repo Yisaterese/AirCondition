be11e9da4998a75f7cb939b3cab04e7c7b633f12
Yisaterese <teresejosephyisa@gmail.com>
2024-02-10 13:27:42 +0100

    first commit

--- television/src/Television.java
+++ television/src/Television.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Television {
+import java.util.ArrayList;
+
+public class Television {
+    private boolean isOn;
+    private int volume;
+    private ArrayList <String> channels = new ArrayList<>();
+    public void  isOn(){
+        isOn = true;
+    }
+    public boolean isPoweredOn(){
+        return isOn;
+    }
+
+    public void isOff(){
+        isOn = false;
+    }
+    public boolean toggleSwitch(){
+        isOn = !isOn;
+        return isOn;
+    }
+
+    public int addVolume(int vol) {
+        for(int counter = 1; counter <= vol; counter++){
+            volume++;
+        }
+        return volume;
+    }
+
+
+    public void subScribeChannels(String... inputChannel){
+        for(int index = 0; index < inputChannel.length; index++) {
+            channels.add(inputChannel[index]);
+        }
+    }
+
+    public void unsubscribeChannels(String... channelName){
+        for(int index = 0; index < channels.size(); index++) {
+            if(channels.get(index).equals(channelName[index])){
+                channels.remove(channelName);
+            }
+        }
+    }
+    public int numberOfChannelsSubscribed(){
+        return channels.size();
+   }
 }
--- television/src/TestTelevision.java
+++ television/src/TestTelevision.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class TestTelevision {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTelevision {
+    @Test
+    public void testTelevisionIsOff_televisionIsOff(){
+        Television tv = new Television();
+        assertFalse(tv.isPoweredOn());
+        tv.isOn();
+        assertTrue(tv.isPoweredOn());
+        tv.isOff();
+        assertFalse(tv.isPoweredOn());
+    }
+    @Test
+    public void testTelevisionIsOn_televisionIsOn(){
+        Television tv = new Television();
+        assertFalse(tv.isPoweredOn());
+        tv.isOn();
+        assertTrue(tv.isPoweredOn());
+        tv.isOff();
+        assertFalse(tv.isPoweredOn());
+        assertTrue(tv.toggleSwitch());
+    }
+
+    @Test
+    public void testTelevisionCanAddVolume_televisionCanAddVolume(){
+        Television tv = new Television();
+        assertFalse(tv.isPoweredOn());
+        tv.isOn();
+        assertTrue(tv.isPoweredOn());
+        assertEquals(2,tv.addVolume(2));
+    }
+
+    @Test
+    public void testTelevisionCanAddChannels_televisionCanAddChannels(){
+        Television tv = new Television();
+        assertFalse(tv.isPoweredOn());
+        tv.isOn();
+        assertTrue(tv.isPoweredOn());
+        assertEquals(2,tv.addVolume(2));
+        tv.subScribeChannels("superSport");
+        assertEquals(1,tv.numberOfChannelsSubscribed());
+    }
+    @Test
+    public void testTelevisionCanAddManyChannels_televisionCanAddManyChannels(){
+        Television tv = new Television();
+        assertFalse(tv.isPoweredOn());
+        tv.isOn();
+        assertTrue(tv.isPoweredOn());
+        assertEquals(2,tv.addVolume(2));
+        tv.subScribeChannels("superSport","Wazobia Tv","NTA","Channels Tv","AIT");
+        assertEquals(5,tv.numberOfChannelsSubscribed());
+    }
+
+    @Test
+    public void testTelevisionCanAddRemoveSomeChannels_televisionCanAddRomeoveSomeChannels(){
+        Television tv = new Television();
+        assertFalse(tv.isPoweredOn());
+        tv.isOn();
+        assertTrue(tv.isPoweredOn());
+        assertEquals(2,tv.addVolume(2));
+        tv.subScribeChannels("superSport","Wazobia Tv","NTA","Channels Tv","AIT");
+        assertEquals(5,tv.numberOfChannelsSubscribed());
+        tv.unsubscribeChannels("superSport","AIT");
+        assertEquals(3,tv.numberOfChannelsSubscribed());
+    }
+
+
 }
+
